fb96f2d70836eb217d45b6d4f2748a7b1380dfcc
Huiqing Zhang <hz9cx@virginia.edu>
2014-11-04 23:13:20 -0500

    Update appointment.rb

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -18,4 +18,13 @@ class Appointment < ActiveRecord::Base
   belongs_to :resident
   belongs_to :physician
   belongs_to :volunteer
+  
+  def self.to_csv
+    CSV.generate do |csv|
+      csv << column_names
+      all.each do |appointments|
+	csv << appointments.attributes.values_at(*column_names)
+      end
+    end
+  end
 end
