c2a130c63e2bd3430a525491831ccab7599de0fd
Huiqing Zhang <hz9cx@virginia.edu>
2014-11-04 23:32:16 -0500

    Update physician.rb

--- app/models/physician.rb
+++ app/models/physician.rb
@@ -14,4 +14,13 @@ include ActionView::Helpers::NumberHelper
 
 class Physician < ActiveRecord::Base
 	has_many :appointments, dependent: :destroy
+	
+	def self.to_csv
+	  CSV.generate do |csv|
+	    csv << column_names
+	    all.each do |physicians|
+	      csv << physicians.attributes.values_at(*column_names)
+	    end
+	  end
+	end
 end
