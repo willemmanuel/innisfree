6438e26141fef9b71fc8f66bec75a7d7ac67e6fe
Huiqing Zhang <hz9cx@virginia.edu>
2014-11-04 23:33:29 -0500

    Update volunteer.rb

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -13,4 +13,13 @@
 class Volunteer < ActiveRecord::Base
   belongs_to :house
   has_many :cars
+  
+  def self.to_csv
+    CSV.generate do |csv|
+      csv << column_names
+      all.each do |volunteers|
+	      csv << volunteers.attributes.values_at(*column_names)
+      end
+    end
+  end
 end
