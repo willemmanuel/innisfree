a9988f2bee7d967e858d093d52fc61ebe541c83e
Robert Emerson <roe2pj@virginia.edu>
2014-11-14 16:19:37 -0500

    Added 5 tests for user controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user_attr = FactoryGirl.attributes_for(:user)
+    @user = User.create!(@user_attr)
+  end
+
+  test "create user" do
+    assert_difference('User.count', 1) do
+      @user_attr[:email] = "test2@example.com"
+      User.create!(@user_attr)
+    end
+  end
+
+  test "successful login" do
+    sign_in(@user)
+    assert_response :success
+  end
+
+  test "user is admin" do
+     assert @user_attr[:admin] == true
+     assert @user.admin == true
+  end
+
+  test "user is approved" do
+    assert @user.approved == true
+  end
+
+  test "delete user" do
+    assert_difference('User.count', -1) do
+      User.delete(@user)
+    end
+  end
+
 end
