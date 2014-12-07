36b5f2e630484670b21e1d6fd7aa0ae89f9913a6
Will Emmanuel <will.emmanuel@gmail.com>
2014-12-07 15:48:54 -0500

    Added appointment digest for all admins

--- config/initializers/email_scheduler.rb
+++ config/initializers/email_scheduler.rb
@@ -2,6 +2,8 @@ scheduler = Rufus::Scheduler.start_new
 
 # Send the digest every day at noon
 scheduler.cron("0 6 * * *") do
-   u = User.where(email: "wre9fz@virginia.edu").first
-   NotificationMailer.appointment_digest(u).deliver
+   admins = User.where(admin: true)
+   admins.each do |admin|
+      NotificationMailer.appointment_digest(admin).deliver
+   end
 end
