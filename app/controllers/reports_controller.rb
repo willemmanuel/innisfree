95ff94639be5f5566f4e89cb54d416aaed38c335
DomenicPuzio <domenicvpuzio@gmail.com>
2014-12-09 16:39:21 -0500

    PDF report generation attempt

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -26,8 +26,16 @@ class ReportsController < ApplicationController
       if params[:date2] != ''
         @appointments = @appointments.where('date <= ?', Date.strptime(params[:date2], "%m/%d/%Y"))
       end
-      flash[:success] = "Report generated successfully"
-      redirect_to Report#index
+      if params[:format] == 'PDF'
+        pdf = Prawn::Document.new(:page_layout => :landscape)
+        pdf.image "#{Rails.root}/app/assets/images/pdf_header.png", width: 400, height: 110, :position => :center
+        pdf.move_down 25
+        printTable(pdf, @appointments)
+        send_data pdf.render, filename: 'Report-'+ Time.now.strftime("%m/%d/%Y") +'.pdf', type: 'application/pdf'
+      else
+        flash[:success] = "Report generated successfully"
+        redirect_to Report#index
+      end
     end
 
    def create
@@ -38,6 +46,40 @@ class ReportsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def report_params
       params.require(:report).permit(:resident_id, :doctor_id, :date,:house_id, :date2)
+    end
+
+    def printTable(pdf, appointments)
+      table_data = [["<b>Date</b>", "<b>Time<b>","<b>Resident<b>", "<b>House<b>", "<b>Volunteer<b>", "<b>Doctor<b>", ]]
+      test = appointments.map
+      test = test.sort_by {|u| [u.date, u.time]}
+      #test = test.sort_by {|u| u.time}
+      test.each do |appointment|
+        table_data +=
+            [[
+
+                 appointment.date.to_formatted_s(:long_ordinal),
+                 appointment.time.strftime("%l:%M %p"),
+                 if not Resident.where('id = ?', appointment.resident_id).blank?
+                   Resident.where('id = ?', appointment.resident_id).first.name
+                 end,
+                 if not Resident.where('id = ?', appointment.resident_id).blank?
+                    id = Resident.where('id = ?', appointment.resident_id).first.house_id
+                    if not House.where('id = ?', id).blank?
+                      House.where('id = ?', id).first.name
+                    end
+                 end,
+                 if not User.where('id = ?', appointment.user_id).blank?
+                   User.where('id = ?', appointment.user_id).first.name
+                 end,
+                 if not Doctor.where('id = ?', appointment.doctor_id).blank?
+                   Doctor.where('id = ?', appointment.doctor_id).first.name
+                 end
+
+             ]]
+      end
+      pdf.table(table_data, :header => true, :cell_style => { :inline_format => true }, \
+                :column_widths => {0 => 140, 1 => 70, 2 => 125, 3 => 125, 4 => 125, 5 => 125}, \
+                :position => :center, :row_colors => ['DDDDDD', 'FFFFFF'])
     end
 
 end
