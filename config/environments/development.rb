aa633f280837a5a43c2466b28cf0142c6fafd995
Innisfree Deployment Server <slp-innisfree@virginia.edu>
2015-04-05 15:08:56 -0600

    Getting the repo up to date with the innisfree deployment. Fixed email bugs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   # config.action_controller.default_url_options = { :trailing_slash => true }
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -37,5 +37,14 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+config.action_mailer.delivery_method = :sendmail
+# Defaults to:
+config.action_mailer.sendmail_settings = { 
+  location: '/usr/sbin/sendmail',
+  arguments: '-i'
+}
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
+# config.action_mailer.default_options = {from: 'noreply <noreply@innisfreevillage.org>'}
+
 end
