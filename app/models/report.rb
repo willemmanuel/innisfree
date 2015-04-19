72dca406152f07475ea3fc345565aae3d1c006d5
Huiqing Zhang <hz9cx@virginia.edu>
2015-04-19 15:59:20 -0400

    comment reports model

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,9 +1,10 @@
 class Report < ActiveRecord::Base
 
   has_paper_trail
-
+  # a one-to-one relationship with appointment, valid appointments must exist in database
   belongs_to :appointment
 
+  # defines the content for CSV list
   def self.to_csv
     CSV.generate do |csv|
       csv << column_names
