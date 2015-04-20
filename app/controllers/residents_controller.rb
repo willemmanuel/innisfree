99b3c0960beec2bc328381e635473e640f9b84ff
Xavier Palathingal <xpalathingal@gmail.com>
2015-04-20 06:10:08 -0400

    Commented residents code

--- app/controllers/residents_controller.rb
+++ app/controllers/residents_controller.rb
@@ -1,11 +1,15 @@
+# The resident controller manages resident data, and serves as the controller to both access and modify data regarding residents. It also verifies privileges for each call when appropriate.
+
 class ResidentsController < ApplicationController
-  before_action :set_resident, only: [:show, :edit, :update, :destroy]
-  before_action :check_admin, only: [:new, :create, :index, :destroy]
-  before_action :check_house, only: [:edit, :update]
-  before_action :set_most_recent, ony: [:new]
-  helper_method :sort_column
+  before_action :set_resident, only: [:show, :edit, :update, :destroy] # loads the specified resident before showing, editing, or deleting
+  before_action :check_admin, only: [:new, :create, :index, :destroy] # makes sure that the user is an admin before creating, destroying, or viewing the management page
+  before_action :check_house, only: [:edit, :update] # makes sure that the user is in the resident's house or an admin before editing
+  before_action :set_most_recent, only: [:new] # loads the most recently created resident if creating
+  helper_method :sort_column # helper method for sorting
 
-
+  # GET /residents
+  # GET /residents.json
+  # index - the main page for resident management for admins, lists all the resident and shows links for details, edit, and delete
   def index
     @residents = Resident.all
     respond_to do |format|
@@ -16,21 +20,25 @@ class ResidentsController < ApplicationController
 
   # GET /residents/1
   # GET /residents/1.json
+  # show - shows the resident and upcoming appointments for resident
   def show
     @upcoming_appointments = Appointment.where('resident_id = ?', @resident.id)
   end
 
   # GET /residents/new
+  # new - creates a new resident object
   def new
     @resident = Resident.new
   end
 
   # GET /residents/1/edit
+  # edit - opens a resident for changes
   def edit
   end
 
   # POST /residents
   # POST /residents.json
+  # create - saves the new resident to database, and shows resident page, or an error if unsuccessful
   def create
     @resident = Resident.new(resident_params)
     respond_to do |format|
@@ -46,6 +54,7 @@ class ResidentsController < ApplicationController
 
   # PATCH/PUT /residents/1
   # PATCH/PUT /residents/1.json
+  # update - saves the edited resident data to database, and shows resident page, or an error if unsuccessful
   def update
     respond_to do |format|
       if @resident.update(resident_params)
@@ -60,6 +69,7 @@ class ResidentsController < ApplicationController
 
   # DELETE /residents/1
   # DELETE /residents/1.json
+  # destroy - removes the resident from database, and shows a success message
   def destroy
     @resident.destroy
     respond_to do |format|
@@ -69,31 +79,35 @@ class ResidentsController < ApplicationController
   end
 
   private
-  # Check to see if the user is an admin (staff)
+  # check_admin - checks to see if the user is an admin (staff)
   def check_admin
     redirect_to houses_path, alert: "You do not have admin privileges." unless current_user.admin
   end
 
-  # Check to see if the user is in the same house as resident or an admin
+  # check_house - checks to see if the user is in the same house as resident or an admin
   def check_house
     redirect_to houses_path, alert: "You do not have access to this house." unless current_user.admin or current_user.house_id == @resident.house_id
   end
 
+  # set_resident - finds the resident with the specified id parameter
   # Use callbacks to share common setup or constraints between actions.
   def set_resident
     @resident = Resident.find(params[:id])
   end
 
+  # set_most_recent - sets the most recently created resident
   def set_most_recent
     @recent = Resident.order("created_at").last
   end
 
+  # resident_params - defines what specific parameters are allowed for a resident
   # Never trust parameters from the scary internet, only allow the white list through.
   def resident_params
     params.require(:resident).permit(:name, :house_id, :notes)
   end
 end
 
+# sort_column - defines how the columns for upcoming appointments are sorted
 def sort_column
   Appointment.column_names.include?(params[:sort]) ? params[:sort] : "resident_id"
   Appointment.column_names.include?(params[:sort]) ? params[:sort] : "doctor_id"
--- app/models/resident.rb
+++ app/models/resident.rb
@@ -12,13 +12,17 @@
 
 class Resident < ActiveRecord::Base
 
+  # Establishes a one-to-many relationship between houses and residents. Each resident has a house where they reside.
   belongs_to :house
+
+  # Validates that each resident has a name as a requirement for creating a resident object
   validates :name, presence: true
-  
+
+  # Specifies what the generated CSV list of residents will look like
   def self.to_csv
     CSV.generate do |csv|
-      csv << column_names
-      all.each do |residents|
+      csv << column_names # headers for the CSV report
+      all.each do |residents| # for each resident, populate the fields specified in column_names
 	csv << residents.attributes.values_at(*column_names)
       end
     end
